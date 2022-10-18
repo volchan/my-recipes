ebfca49ccc0d49d198fc5785a34a987358c05d5c
Stephane DE LA TORRE <s.dlt@hotmail.fr>
2022-10-18 16:55:47 +0200

    feat: add User entity

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
