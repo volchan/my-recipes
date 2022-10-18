e99d9b67499e8c3e21ce647592d2151c580b2a4f
Stephane DE LA TORRE <s.dlt@hotmail.fr>
2022-10-18 16:50:31 +0200

    feat: add User entity

---
+++ apps/server/src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [
+    AuthService,
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+    UsersService,
+  ],
+})
+export class UsersModule {}
